4b986b05f3ab82fa27f8be4d838458fc1effa3bd
Mirror83 <glenochieng045@gmail.com>
2024-06-30 07:41:46 +0300

    Implemented depth-first and breadth-first traversal algorithms

---
+++ src/main/kotlin/Traversals.kt
@@ -0,0 +1,53 @@
+package org.example
+
+
+val graph = mapOf(
+    "a" to listOf("b", "c"),
+    "b" to listOf("d"),
+    "c" to listOf("e"),
+    "d" to listOf("f"),
+    "e" to listOf(),
+    "f" to listOf(),
+
+)
+fun depthFirstPrint(graph: Map<String, List<String>>, source: String) {
+    val stack = mutableListOf(source)
+
+    while (stack.isNotEmpty()) {
+        val current = stack.removeLast()
+        println(current)
+        for (neighbour in graph[current]!!) {
+            stack.add(neighbour)
+        }
+    }
+}
+
+fun depthFirstPrintRecursive(graph: Map<String, List<String>>, source: String) {
+    println(source)
+
+    // Implicit base case is where source has no neighbours
+    for (neighbour in graph[source]!!) {
+        depthFirstPrintRecursive(graph, neighbour)
+    }
+}
+
+fun breadthFirstPrint(graph: Map<String, List<String>>, source: String) {
+    val queue = mutableListOf(source)
+
+    while (queue.isNotEmpty()) {
+        val current = queue.removeFirst()
+        println(current)
+
+        for (neighbour in graph[current]!!) {
+            queue.add(neighbour)
+        }
+    }
+}
+
+fun main() {
+    depthFirstPrint(graph, "a")  // acebdf
+    println()
+    depthFirstPrintRecursive(graph, "a")  // abdfce
+    println()
+    breadthFirstPrint(graph, "a")  // abcdef
+}
